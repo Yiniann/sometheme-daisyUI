2f5b85d836d457e93fffe85184f6b0c13d0613f5
Nian Yi <i@en1an.com>
2025-05-23 09:47:11 +0800

    helmet

--- src/App.jsx
+++ src/App.jsx
@@ -13,54 +13,65 @@ import Order from "./pages/Order";
 import Ticket from "./pages/Ticket";
 import Profile from "./pages/Profile";
 
-import { useThemeInit } from "./hooks/useThemeInit";
-import ProtectedRoute from "./components/ProtectedRoute";
+import { useThemeInit } from "./hooks/useThemeInit";//主题初始化
+import ProtectedRoute from "./components/ProtectedRoute";//保护路由
+import { HelmetProvider,Helmet } from "react-helmet-async";//Helmet
+import { getValue } from "./config/runtimeConfig";
 
 const App = () => {
+  const siteName = getValue("siteName","App")
+  const favicon = getValue("favicon","")
   const isAuthenticated = useSelector(
     (state) => state.passport.isAuthenticated
   );
   useThemeInit()
 
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* 登录页：未登录可访问，已登录跳转首页 */}
-        <Route
-          path="/login"
-          element={
-            isAuthenticated ? <Navigate to="/home" replace /> : <LoginLayout />
-          }
-        >
-          <Route index element={<Login />} />
-        </Route>
+     <HelmetProvider>
+      <Helmet>
+        <title>{siteName}</title>
+        <meta name="description" content="Network Server" />
+        <link rel="icon" href={favicon} />
+      </Helmet>
+      <BrowserRouter>
+        <Routes>
+          {/* 登录页：未登录可访问，已登录跳转首页 */}
+          <Route
+            path="/login"
+            element={
+              isAuthenticated ? <Navigate to="/home" replace /> : <LoginLayout />
+            }
+          >
+            <Route index element={<Login />} />
+          </Route>
 
-        {/* 主应用：仅登录后可访问 */}
-        <Route
-          element={
-            <ProtectedRoute>
-              <AppLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<Home />} />
-          <Route path="home" element={<Home />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="plan" element={<Plan />} />
-          <Route path="order" element={<Order />} />
-          <Route path="ticket" element={<Ticket />} />
-          <Route path="profile" element={<Profile />} />
-        </Route>
+          {/* 主应用：仅登录后可访问 */}
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="plan" element={<Plan />} />
+            <Route path="order" element={<Order />} />
+            <Route path="ticket" element={<Ticket />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
 
-        {/* 404 页面 */}
-        <Route
-          path="*"
-          element={
-            isAuthenticated ? <NotFound /> : <Navigate to="/login" replace />
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+          {/* 404 页面 */}
+          <Route
+            path="*"
+            element={
+              isAuthenticated ? <NotFound /> : <Navigate to="/login" replace />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 };
 
