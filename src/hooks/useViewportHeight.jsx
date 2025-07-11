4c270e8de908c0a4d6527ec11a57e2b8fe33f674
yinian <i@en1an.com>
2025-07-11 13:28:34 +0800

    输入时放大问题

--- src/hooks/useViewportHeight.jsx
+++ src/hooks/useViewportHeight.jsx
@@ -1,23 +1,11 @@
 import { useState, useEffect } from 'react'
 
 const useViewportHeight = () => {
-  const [vh, setVh] = useState(window.innerHeight * 0.01)
+  const [vh, setVh] = useState(document.documentElement.clientHeight * 0.01)
 
   useEffect(() => {
-    const updateVh = () => {
-      setVh(window.innerHeight * 0.01)
-    }
-
-    window.addEventListener('resize', updateVh)
-    window.addEventListener('orientationchange', updateVh)
-
-    // 初始化
-    updateVh()
-
-    return () => {
-      window.removeEventListener('resize', updateVh)
-      window.removeEventListener('orientationchange', updateVh)
-    }
+    const vh = document.documentElement.clientHeight * 0.01
+    setVh(vh)
   }, [])
 
   return vh
